12c578eb73c2f6ca1fda77bba923011a3cee193e
Vladimir Shataev <shataevvova@yandex.ru>
2019-03-24 14:02:32 +0300

    1.3. JavaDoc. Code convention[#121278]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,17 @@
 package ru.job4j.calculate;
+
+/**
+ * Calculate.
+ *
+ *@author Vladimir Shataev (shayaevvova@yandex.ru)
+
+
+
 public class Calculate{
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 	System.out.println("Hello World");}
 }
