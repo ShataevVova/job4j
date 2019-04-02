6e2d6bffb83becd03876eabde6304ff9e7b4a1f2
Vladimir Shataev <shataevvova@yandex.ru>
2019-04-03 00:26:39 +0400

    3.1. Элементарный калькулятор.[#121268]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculator;
+
+/**
+ * Calculator.
+ *
+ *@author Vladimir Shataev (shayaevvova@yandex.ru)
+
+*/
+
+public class Calculator {
+	/**
+	* Сложение
+	*/
+	public double add(double first, double second) {
+		return first + second;
+	}
+	public double subtract(double first, double second) {
+		return first - second;
+	}
+	public double div(double first, double second) {
+		return first/second;
+	}
+	public double multiple(double first, double second) {
+		return first*second;
+	}
+}
