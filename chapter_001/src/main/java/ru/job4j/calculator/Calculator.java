63686399b21470b4750bb0bad2659d761a100c7a
Vladimir Shataev <shataevvova@yandex.ru>
2019-04-05 01:00:37 +0400

    3.2. Конвертер валюты.[#121264]
    3.1. Элементарный калькулятор.[#121268]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -18,9 +18,9 @@ public class Calculator {
 		return first - second;
 	}
 	public double div(double first, double second) {
-		return first/second;
+		return first / second;
 	}
 	public double multiple(double first, double second) {
-		return first*second;
+		return first * second;
 	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/converter/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for converter task.
+*
+* @author Vladimir Shataev (shataevvova@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.converter;
