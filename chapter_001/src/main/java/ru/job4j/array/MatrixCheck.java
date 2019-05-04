67e06c63ef24f505845722884f9804a28008965f
Shataev Vladimir <shataevvova@yandex.ru>
2019-05-05 00:08:25 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.[#121257]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -5,52 +5,26 @@ import java.util.Arrays;
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
-        boolean leftDiagonal = true;
-        boolean rightDiagonal = true;
-        System.out.println("Заданный массив: " + Arrays.deepToString(data) + "\t");
-        System.out.println("Размер массива: " + data.length + "x" + data[0].length);
-        // перебор по строкам.
         for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[0].length; j++) { // перебор по столбцам.
-                if (i == j) { // рассматриваются только элемены по диагонали матрицы.
-                    System.out.println("в индексе " + i + j + " содержится " + data[i][j] + "*");
-                    System.out.println("в индексе " + (data.length - i - 1) + j + " содержится " + data[data.length - i - 1][j] + "| " + " ");
-                    if ((data[0][0] != data[data.length - i - 1][j])) {
-                        rightDiagonal = false;
-                        break;
-                    }
-                    if ((data[0][0] != data[i][j])) {
-                        leftDiagonal = false;
-                        break;
-                    }
-                }
+            if (data[0][0] != data[i][i]) {
+                result = false;
+                break;
+            }
+            if (data[data.length - 1][0] != data[i][data.length - i - 1]) {
+                result = false;
+                break;
             }
         }
-        if (rightDiagonal) {
-            System.out.println("Правая диагональ массива содержит одинаковые значения: (" + data[0][0] + ") => " + rightDiagonal);
+        System.out.println("Массив размером " + data.length + "x" + data.length);
+        System.out.println(Arrays.deepToString(data));
+        if (result) {
+            System.out.println("Диагонали содержат одинаковые значения ");
         } else {
-            System.out.println("Правая диагональ массива содержит разные значения => " + rightDiagonal);
-        }
-        if (leftDiagonal) {
-            System.out.println("Левая диагональ массива содержит одинаковые значения: (" + data[0][0] + ") => " + leftDiagonal);
-        } else {
-            System.out.println("Левая диагональ массива содержит разные значения => " + leftDiagonal);
-        }
-
-        if (!(rightDiagonal && leftDiagonal)) {
-            result = false;
-            System.out.println("Левые и правые диагонали ОТЛИЧАЮТСЯ друг от друга (" + leftDiagonal + " && " + rightDiagonal  + " = " + result + ")");
-            System.out.println("=> Диагонали массива НЕ СОДЕРЖАТ ОДИНАКОВЫЕ ЗНАЧЕНИЯ элементов ");
-            System.out.println();
-        } else {
-            System.out.println("Левые и правые диагонали ОДИНАКОВЫ (" + leftDiagonal + " && " + rightDiagonal  + " = " + result + ")");
-            System.out.println("=> Диагонали массива СОДЕРЖАТ ОДИНАКОВЫЕ ЗНАЧЕНИЯ элементов ");
-            System.out.println();
+            System.out.println("Диагонали содержат разные значения ");
         }
         return result;
     }
 }
-
 
 
 
